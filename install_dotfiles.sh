dfe7ae91172f8d99844b1dfd7d6ef53f9351b313
Mike Nichols <nichols.mike.s@gmail.com>
2011-05-18 10:04:07 -0700

    updating shell

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
+
+VIMDIR=~/.vim
+VIMBACKUP=~/.vim_bak
+GVIMRC=~/.gvimrc
+VIMRC=~/.vimrc
+
 cd ~
+
+if [ ! -a "$GVIMRC" ]
+then
+touch $GVIMRC
+fi
+
+if [ ! -a "$VIMRC" ]
+then
+touch $VIMRC
+fi
+
+if [ -d "$VIMDIR" ]
+then
+echo "backing up old .vim directory, with .vimrc and .gvimrc inside"
+mv $VIMRC $VIMDIR/.vimrc
+mv $GVIMRC $VIMDIR/.gvimrc
+mv $VIMDIR $VIMBACKUP
+fi
+
+
 git clone http://github.com/mnichols/vim-dotfiles.git ~/.vim
 ln -s ~/.vim/vimrc ~/.vimrc
 ln -s ~/.vim/gvimrc ~/.gvimrc
